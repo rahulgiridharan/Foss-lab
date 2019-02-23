f8fb4e1be60222b1959bbf48045d2b150e7e3332
rahulgiridharan <rahul.giridharan@cet.ac.in>
2019-02-23 15:36:50 +0530

    RevertedandUpdatedTwice

--- foss.c
+++ foss.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main()
 {
-printf("hello world, the local copy has been updated");
+printf("hello world, the local copy has been updated twice and the commit is now Reverted");
 return 0;
 }
